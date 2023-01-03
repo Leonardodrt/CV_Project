c97b4054502d83f4766797d1c08c54539f30a40a
mdjdgabe <115570003+mdjdgabe@users.noreply.github.com>
2023-01-03 19:44:47 +0000

    Kitchen implementada (for the most part)

---
+++ scripts/bread.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+	var animation_player = get_node("AnimationPlayer")
+	animation_player.play("Knife")
+	animation_player.connect("animation_finished", animation_player, "play")
